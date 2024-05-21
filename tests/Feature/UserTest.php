7b3cd2fc9a3b38a7df27d448b91518f25b5d98be
carlosjuanco <carlosjuancho328@gmail.com>
2024-05-20 19:39:26 -0600

    Crear las pruebas de validacion de usuarios Establecer que el campo role_id sea obligatorio Crear la carpeta de idioma espanol Configurar el idioma predeterminado a espanol Eliminar los dos archivos de pruebas que vienen por defecto General error: 1364 Field role_id

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -4,73 +4,146 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Tests\TestCase;
+
+use App\Models\User;
 
 class UserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    
     /**
      * Crear un usuario
      *
      * @return void
      */
-    // public function test_new_user()
-    // {
-    //     $user = User::where('role_id', 1)->first();
+    public function test_new_user()
+    {
+        $user = User::where('role_id', 1)->first();
 
-    //     $response = $this->actingAs($user)->post('api/users', [
-    //         'name' => 'Adriana Santiago Morales', 
-    //         'email' => Str::random(2) . "@gmail.com",
-    //         'password' => Hash::make('password'),
-    //         'role_id' => 1 
-    //     ]);
+        $email = Str::random(2) . "@gmail.com";
+        $response = $this->actingAs($user)->post('api/users', [
+            'name' => 'Adriana Santiago Morales', 
+            'email' => $email,
+            'password' => Str::random(10),
+            'role_id' => 1
+        ]);
 
-    //     $response->assertStatus(200)
-    //     ->assertJson(['message' => 'Usuario creado correctamente']);
+        $response->assertStatus(200)
+        ->assertJson(['message' => 'Usuario creado correctamente']);
 
-    //     $this->post('api/logout');
-    // }
+        // Verificar que el usuario fue creado en la base de datos
+        $this->assertDatabaseHas('users', [
+            'email' => $email,
+            'role_id' => 1
+        ]);
+
+        $this->post('api/logout');
+    }
 
     /**
-     * Comprobar que el nombre juancho de usuario sea unico
+     * Comprobar que el campo email sea único
      *
      * @return void
      */
-    // public function test_verify_name_user_unique()
-    // {
-    //     $user = User::where('role_id', 1)->first();
+    public function test_verify_the_field_email_unique()
+    {
+        $user = User::where('role_id', 1)->first();
 
-    //     $response = $this->actingAs($user)->post('api/users', [
-    //         'name' => 'juan', 
-    //         'last_name' => 'rojas', 
-    //         'second_last_name' => 'garcia', 
-    //         'username' => 'juancho', 
-    //         'role_id' => 1 
-    //     ]);
-    //     $response->assertStatus(302)
-    //     ->assertSessionHasErrors(['username' => 'El campo usuario ya ha sido registrado.']);
-    //     $this->post('api/logout');
-    // }
+        $response = $this->actingAs($user)->post('api/users', [
+            'name' => 'juan', 
+            'email' => 'carlosjuancho328@gmail.com',
+            'password' => Str::random(10),
+            'role_id' => 1 
+        ]);
 
-    // public function test_verify_name_is_required()
-    // {
-    //     $user = User::where('role_id', 1)->first();
+        $response->assertStatus(302)
+        ->assertSessionHasErrors(['email' => 'El correo ya ha sido registrado.']);
 
-    //     $response = $this->actingAs($user)->post('api/users', [
-    //         'last_name' => 'rojas', 
-    //         'second_last_name' => 'garcia', 
-    //         'username' => Str::random(10), 
-    //         'role_id' => 1 
-    //     ]);
-    //     // printf($response->assertStatus);
-    //     $response->assertStatus(302)
-    //     ->assertSessionHasErrors(['name' => 'El campo nombre es obligatorio.']);
-    //     $this->post('api/logout');
-    // }
+        $this->post('api/logout');
+    }
+
+    /**
+     * Comprobar que el campo nombre sea requerido
+     *
+     * @return void
+     */
+    public function test_verify_name_is_required()
+    {
+        $user = User::where('role_id', 1)->first();
+
+        $response = $this->actingAs($user)->post('api/users', [
+            'email' => Str::random(2) . "@gmail.com",
+            'password' => Str::random(10), 
+            'role_id' => 1 
+        ]);
+
+        $response->assertStatus(302)
+        ->assertSessionHasErrors(['name' => 'El campo nombre es obligatorio.']);
+
+        $this->post('api/logout');
+    }
+
+    /**
+     * Comprobar que el campo email sea requerido
+     *
+     * @return void
+     */
+    public function test_verify_email_is_required()
+    {
+        $user = User::where('role_id', 1)->first();
+
+        $response = $this->actingAs($user)->post('api/users', [
+            'name' => 'juan',
+            'password' => Str::random(10), 
+            'role_id' => 1 
+        ]);
+
+        $response->assertStatus(302)
+        ->assertSessionHasErrors(['email' => 'El campo correo es obligatorio.']);
+
+        $this->post('api/logout');
+    }
+
+    /**
+     * Comprobar que el campo role_id sea requerido
+     *
+     * @return void
+     */
+    public function test_verify_role_id_is_required()
+    {
+        $user = User::where('role_id', 1)->first();
+
+        $response = $this->actingAs($user)->post('api/users', [
+            'name' => 'juan',
+            'email' => Str::random(2) . "@gmail.com",
+            'password' => Str::random(10), 
+        ]);
+
+        $response->assertStatus(302)
+        ->assertSessionHasErrors(['role_id' => 'El campo role id es obligatorio.']);
+
+        $this->post('api/logout');
+    }
+
+    /**
+     * Comprobar que el campo role_id exista en la tabla roles
+     *
+     * @return void
+     */
+    public function test_check_that_the_role_id_field_exists_in_the_roles_table()
+    {
+        $user = User::where('role_id', 1)->first();
+
+        $response = $this->actingAs($user)->post('api/users', [
+            'name' => 'garcia',
+            'email' => Str::random(2) . "@gmail.com",
+            'password' => Str::random(10),
+            'role_id' => 1000
+        ]);
+
+        $response->assertStatus(302)
+        ->assertSessionHasErrors(['role_id' => 'role id no existe.']);
+
+        $this->post('api/logout');
+    }
 }
