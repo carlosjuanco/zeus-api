ddf78a19459a98b54daac9df4963a9140eef4234
carlosjuanco <carlosjuancho328@gmail.com>
2024-05-29 16:17:01 -0600

    Retornar pagina principal para el rol Creadores del sistema

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,11 +19,16 @@ class AuthController extends Controller
 
         if (Hash::check($data['password'], $user->password)) {
             $pages_permissions = [];
+            $pages = $user->role->pages;
+
+            foreach ($pages as $page) {
+                array_push($pages_permissions, $page->name);
+            }
 
             $api_token = $user->createToken('api_token', $pages_permissions);
             $api_token = $api_token->plainTextToken;
 
-            return response()->json(compact('api_token', 'user'), 200);
+            return response()->json(compact('api_token', 'user', 'pages'), 200);
         }
 
         return response()->json([
@@ -35,7 +40,8 @@ class AuthController extends Controller
 
     public function check (Request $request) {
         return response()->json([
-            'user' => $request->user()
+            'user' => $request->user(),
+            'pages' => $request->user()->role->pages
         ], 200);
     }
 
