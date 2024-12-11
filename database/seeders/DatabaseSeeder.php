fb3bd6af5c1609b768f9c848069a7ace5a81a1df
carlosjuanco <carlosjuancho328@gmail.com>
2024-12-11 16:06:29 -0600

    IAS-6 Mostrar el mensaje de Bienvenido al sistema

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             PageSeeder::class,
             PageRolesSeeder::class,
-            AddComponentNameInformationInVueSeeder::class
+            AddComponentNameInformationInVueSeeder::class,
+            AddNewPageWelcomeUserSeeder::class,
         ]);
     }
 }
