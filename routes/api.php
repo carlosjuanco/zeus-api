ce2fa8ac2f3377e01cb57a0881f9c00c27c619f3
carlosjuanco <carlosjuancho328@gmail.com>
2026-04-24 22:29:38 -0600

    SS0-26 Validar las reglas de negocio del componente comunidades

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CommunityController;
 
 Route::post('login', [AuthController::class, 'login']);
 
@@ -19,3 +20,15 @@ Route::controller(UserController::class)
     ->group(function () {
         Route::post('users', 'store');
 });
+
+Route::get('index', [CommunityController::class, 'index'])
+    ->middleware(['auth:sanctum', 'can:index,App\Models\Community']);
+
+Route::post('store', [CommunityController::class, 'store'])
+    ->middleware(['auth:sanctum', 'can:create,App\Models\Community']);
+
+Route::put('update/{community}', [CommunityController::class, 'update'])
+    ->middleware(['auth:sanctum', 'can:update,community']);
+    
+Route::delete('destroy/{community}', [CommunityController::class, 'destroy'])
+    ->middleware(['auth:sanctum', 'can:delete,community']);
