91ad50c002fe95003e313039890f7f249b85191f
carlosjuanco <carlosjuancho328@gmail.com>
2026-06-06 21:54:25 -0600

    SS0-41 Pruebas unitarias del componente Comunidades

--- tests/Feature/3.-CommunityTest.php
+++ tests/Feature/3.-CommunityTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 
 use App\Models\User;
 use App\Models\Human;
+use App\Models\Community;
 
 class CommunityTest extends TestCase
 {
@@ -40,6 +41,44 @@ class CommunityTest extends TestCase
             'human_id' => $administrativeUser->id
         ]);
 
+        $this->post('api/logout');
+    }
+
+    /**
+     * Afirmar que se puede editar una comunidad
+     * 
+     * @return boolean
+     */
+    public function test_assert_that_a_community_can_be_edited()
+    {
+        //  Consultamos el segundo usuario que tiene el rol Administrativo.
+        $userAdministrative = User::where('role_id', 2)->first();
+
+        //  Consultamos la comunidad "San Pedro Cholula" y agregamos
+        //  una letra "a" demás, para comprobar que se puede editar
+        $communitySanPedroCholula = Community::where('name', 'San Pedro Cholula')->first();
+
+        $updatedName = 'San Pedro Cholula a';
+
+        //  Ejecutar la petición
+        $response = $this->actingAs($userAdministrative)
+            ->put('api/communities/' . $communitySanPedroCholula->id, [
+                'name' => $updatedName
+            ]);
+
+        //  Verificar resultados
+        $response->assertStatus(200)
+            ->assertJson([
+                'message' => '¡Listo! Tus datos se guardaron bien.',
+            ]);
+
+        //  Verificar en la base de datos
+        $this->assertDatabaseHas('communities', [
+            'id' => $communitySanPedroCholula->id,
+            'name' => $updatedName
+        ]);
+
+        //  Cerrar sesión
         $this->post('api/logout');
     }
 }
