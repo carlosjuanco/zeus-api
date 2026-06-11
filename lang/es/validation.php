ce530615028dcfe4dec3a81d6b015ad5523b3d9a
carlosjuanco <carlosjuancho328@gmail.com>
2026-06-10 18:18:28 -0600

    SS0-43 Mensajes de validacion

--- lang/es/validation.php
+++ lang/es/validation.php
@@ -72,13 +72,14 @@ return [
     'lte'                  => [
         'numeric' => 'El campo :attribute debe ser como máximo :value.',
         'file'    => 'El campo :attribute debe tener como máximo :value kilobytes.',
-        'string'  => 'El campo :attribute debe tener como máximo :value caracteres.',
+        'string'  => 'El campo :attribute debe tener como máximo :value 
+        .',
         'array'   => 'El campo :attribute debe tener como máximo :value elementos.',
     ],
     'max'                  => [
         'numeric' => ':attribute no debe ser mayor que :max.',
         'file'    => ':attribute no debe ser mayor que :max kilobytes.',
-        'string'  => ':attribute no debe ser mayor que :max caracteres.',
+        'string'  => 'El campo :attribute no debe ser mayor que :max caracteres.',
         'array'   => ':attribute no debe tener más de :max elementos.',
     ],
     'mimes'                => ':attribute debe ser un archivo con formato: :values.',
